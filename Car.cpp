bfcbe077b70b6751e2c68962ed555227a026254a
Shane <shane20111212@gmail.com>
2022-10-28 15:41:32 +1030

    fixed qs1

--- Car.cpp
+++ Car.cpp
@@ -7,3 +7,27 @@ Car::Car(int price){
     vinNumber = 0;
     emissions = 0;
 }
+
+int Car::get_price(){
+    return price;
+}
+
+int Car::get_vinNumber(){
+    return vinNumber;
+}
+
+int Car::get_emissions(){
+    return emissions;
+}
+
+int Car::set_price(int p){
+   price = p; 
+}
+
+int Car::set_vinNumber(int vin){
+    vinNumber = vin;
+}
+
+int Car::set_emissions(int e){
+    emissions = e;
+}
--- Car.h
+++ Car.h
@@ -6,5 +6,11 @@ class Car{
     public:
         Car(int price);
         virtual void drive(int kms) = 0;
+        int get_price();
+        int get_vinNumber();
+        int get_emissions();
+        int set_price(int p);
+        int set_vinNumber(int vin);
+        int set_emissions(int e);
         ~Car();
 };
